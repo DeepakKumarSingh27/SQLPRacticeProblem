9f61cab2ef53a60ab1dc280f1b49f8138afba603
DeepakKumarSingh27 <69988805+DeepakKumarSingh27@users.noreply.github.com>
2023-12-14 10:55:45 +0530

    All functions and All store procedure problems with theory completed

--- EmployeePayrollSQLQuery2.sql
+++ EmployeePayrollSQLQuery2.sql
@@ -374,12 +374,366 @@ ON E1.FirstName = E2.FirstName;
 Select * from Salary;
 
 
+/*Store procedure Error */
+USE Employee_payroll
+GO
+-- Basic example of TRY...CATCH
+ 
+BEGIN TRY
+-- Generate a divide-by-zero error  
+  SELECT
+    1 / 0 AS Error;
+END TRY
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_STATE() AS ErrorState,
+    ERROR_SEVERITY() AS ErrorSeverity,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_LINE() AS ErrorLine,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
+GO
+
+-- Table to record errors
+ 
+CREATE TABLE DB_Errors
+         (ErrorID        INT IDENTITY(1, 1),
+          UserName       VARCHAR(100),
+          ErrorNumber    INT,
+          ErrorState     INT,
+          ErrorSeverity  INT,
+          ErrorLine      INT,
+          ErrorProcedure VARCHAR(MAX),
+          ErrorMessage   VARCHAR(MAX),
+          ErrorDateTime  DATETIME)
+GO
+
+
+ALTER PROCEDURE DB_Errors @employeeid INT,
+                   @productid  INT,
+                   @quantity   SMALLINT,
+                   @saleid     UNIQUEIDENTIFIER OUTPUT
+AS
+SET @saleid = NEWID()
+  BEGIN TRY
+    INSERT INTO Sales.Sales
+         SELECT
+           @saleid,
+           @productid,
+           @employeeid,
+           @quantity
+  END TRY
+  BEGIN CATCH
+    INSERT INTO dbo.DB_Errors
+    VALUES
+  (SUSER_SNAME(),
+   ERROR_NUMBER(),
+   ERROR_STATE(),
+   ERROR_SEVERITY(),
+   ERROR_LINE(),
+   ERROR_PROCEDURE(),
+   ERROR_MESSAGE(),
+   GETDATE());
+  END CATCH
+GO
+
+
+select * from Employee
+
+CREATE PROCEDURE SpGetEmployee
+AS
+BEGIN
+select FirstName, Gender from Employee order by FirstName
+END
+
+ALTER PROCEDURE SpGetEmployee
+AS
+BEGIN
+select FirstName, Gender from Employee order by FirstName
+END
+
+ exec SpGetEmployee;
+
+ CREATE PROCEDURE SpGetEmployeesByGenderAndDept
+    @EmployeeID INT,
+    @Gender CHAR(1)
+ AS
+ BEGIN
+ SELECT FirstName,Gender,EmployeeID from Employee
+ where EmployeeID = @EmployeeID and  Gender = @Gender
+ END
+ Select * from Employee
+
+ SpGetEmployeesByGenderAndDept 1001,'m'
+
+ sp_helptext SpGetEmployeesByGenderAndDept
+
+ ALTER PROCEDURE SpGetEmployeesByGenderAndDept
+    @EmployeeID INT,
+    @Gender CHAR(1)
+	with Encryption
+ AS
+ BEGIN
+ SELECT FirstName,Gender,EmployeeID from Employee
+ where EmployeeID = @EmployeeID and  Gender = @Gender
+ END
+
+ CREATE PROCEDURE SpGetEmployeeCountByGender
+ @Gender CHAR(1),
+ @EmployeeCount INT OUTPUT
+ as
+ Begin
+ SELECT @EmployeeCount = Count(EmployeeID) from Employee where Gender = @Gender
+ End
+
+ Declare @TotalCount INT
+ Execute SpGetEmployeeCountByGender 'm',@TotalCount OUTPUT
+ print @TotalCount
+
+
+ SELECT Count(*) from Employee;
+
+
+ sp_depends SpGetEmployeeCountByGender
+ 
+
+ CREATE PROC spGetTotalCount
+ @TotalCount int out
+ As
+ Begin
+ select @TotalCount = Count(EmployeeID) from Employee;
+ End
+
+ CREATE PROC spGetTotalCount2
+ As
+ Begin
+  return (select Count(EmployeeID) from Employee);
+ End
+
+ Declare @Total int
+ Execute @Total = spGetTotalCount2
+ print @Total
+
+ spGetTotalCount
+
+ Declare @Total int
+ Execute spGetTotalCount @Total out
+ print @Total
+
+ CREATE PROC spGetNumberByID1
+ @EmployeeID INT,
+ @Name varchar(20) out
+ as
+ Begin
+ select @Name = FirstName  from Employee where EmployeeID = @EmployeeID
+ End
+
+ Declare @Name varchar(20)
+ Execute spGetNumberByID1 1001,@Name out
+ print 'Name = '+@Name
+
+
+ CREATE PROC spGetNumberByID2
+ @EmployeeID INT
+ as
+ Begin
+  return (select FirstName  from Employee where EmployeeID = @EmployeeID)
+ End
+
+
+  Declare @Name varchar(20)
+ Execute @Name = spGetNumberByID2 1001
+ print 'Name = '+@Name
+
+ CREATE DATABASE ERRORHANDLING;
+ USE ERRORHANDLING
+ CREATE TABLE TBLpRODUCT(PRODUCTID INT PRIMARY KEY,NAME VARCHAR(30),QTYAVAILABLE INT);
+ INSERT INTO TBLpRODUCT(PRODUCTID,NAME,QTYAVAILABLE)
+ VALUES
+ (1,'Laptop',90),
+ (2,'Desktops',50);
+ select * from TBLpRODUCT;
+
+ CREATE TABLE tblProductSales(productSalesid int primary key,PRODUCTID INT,QuantitySold int);
+ INSERT INTO tblProductSales(productSalesid,PRODUCTID,QuantitySold)
+ values
+ (1,1,10),
+ (2,1,10);
+ 
+     ALTER TABLE tblProductSale
+	 ADD  FOREIGN KEY (PRODUCTID) REFERENCES TBLpRODUCT(PRODUCTID);
+
+ create procedure spSellproduct
+ @PRODUCTID int,
+ @QuantityToSell int
+ as
+ Begin
+ --check stock
+ Declare @StockAvailable int
+ SELECT @StockAvailable = QtyAvailable
+ from TBLpRODUCT where PRODUCTID = @PRODUCTID
+ if(@StockAvailable < @QuantityToSell)
+ Begin
+ Raiserror('not enough stock availabe',16,1)
+ end
+ else
+ Begin
+ Begin Tran
+ Update TBLpRODUCT set QTYAVAILABLE = (QTYAVAILABLE - @QuantityToSell)
+ where PRODUCTID = @PRODUCTID
+
+ Declare @MaxProductSalesId int
+ select @MaxProductSalesId = case when 
+          MAX(ProductSalesID) is null
+		  then 0 else Max(ProductSalesId) end
+		  from tblProductSales
+		  set @MaxProductSalesId = @MaxProductSalesId + 1
+		  insert into tblProductSales values (@MaxProductSalesId,@PRODUCTID,@QuantityToSell)
+		  commit tran
+		  END
+END
+
+Execute spSellproduct 1,10
+
+select * from tblProductSales
+select * from TBLpRODUCT
+
+create Proc employeeDetails
+as
+select * from employee
+go
+
+
+create Proc employeeDetails
+as
+select * from Employee
+go
+use Employee_payroll
+employeeDetails
+
+create Proc empGender
+@Gender Char(1)
+as
+select * from Employee
+where Gender = @Gender
+go
+empGender 'f'
+create view frmalEmp
+as
+select * from Employee
+where Gender = 'f'
+frmalEmp
+select * from frmalEmp;
+
+drop view frmalEmp
+
+declare @val1 int
+declare @val2 int
+begin try
+set @val1 = 8;
+set @val2 = @val1/0;
+end try
+begin catch
+print error_message()
+end catch
 
 
 
+begin try
+select EmployeeID + FirstName from Employee
+end try
+begin catch
+print 'cannot add num value with str value'
+end catch
+go
+
+select * from Employee
+
+Declare @age int;
+Begin try
+set @age = 60;
+print 'Line 1';
+print 'Line 2';
+IF @age > 40
+THROW 5000,'Age is greater then 40',16;
+print 'Line 3';
+print 'Line 4';
+End try
+Begin catch
+print'Inside the catch block'
+print Concat('Error_message()=',Error_message());
+print Concat('Error_Line()=',Error_Line());
+print Concat('Error_Number()=',Error_Number());
+print Concat('Error_Severity()=',Error_Severity());
+print Concat('Error_State()=',Error_State());
+print Concat('Error_Procedure()=',Error_procedure());
+end catch
 
 
+/*Assuming we want to create a stored procedure to calculate the total salary
+for a specific employee based on their EmployeeID and a provided PayrollPeriodID.(handle exceptions in SP)*/
 
+CREATE PROCEDURE CalculateTotalSalary
+    @EmployeeID INT,
+    @PayrollPeriodID INT,
+    @TotalSalary DECIMAL OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    BEGIN TRY
+        SELECT @TotalSalary = SUM(BasicSalary) 
+		AS TotalSalary
+        FROM Salary
+        WHERE EmployeeID = @EmployeeID
+        AND Salaryid = @PayrollPeriodID;
 
+        IF @TotalSalary IS NULL
+            SET @TotalSalary = 0;
+    END TRY
+    BEGIN CATCH
+        SET @TotalSalary = -1;
+    END CATCH
+END;
 
+/*  Write a Stored Procedure to validate username
+and password raise error if the data already present.*/
+
+CREATE PROCEDURE ValidateUsernamePassword
+    @Username VARCHAR(50),
+    @Password VARCHAR(50)
+AS
+BEGIN
+    DECLARE @ExistingCount INT;
+    SELECT @ExistingCount = COUNT(*)
+    FROM Employee
+    WHERE Username = @Username AND Password = @Password;
+
+    -- If the combination exists, raise an error
+    IF @ExistingCount > 0
+    BEGIN
+        RAISERROR('Username and password combination already exists!', 16, 1);
+        RETURN;
+    END
+	END
+	/*create a stored procedure that will take the
+Employee ID of a person and checks if it is in the table. There are two
+conditions*/
+
+CREATE PROCEDURE CheckEmployeeExistence
+    @EmployeeID INT,
+    @Exists int OUTPUT
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM Employee WHERE EmployeeID = @EmployeeID)
+        SET @Exists = 1; -- EmployeeID exists in the table
+    ELSE
+        SET @Exists = 0; -- EmployeeID does not exist in the table
+END;
+CheckEmployeeExistence 1001
+
+Declare @Exist int
+Execute @Exist = CheckEmployeeExistence 1001;
+print @Exist
 
